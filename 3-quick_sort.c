aa20b1695b7cb6e996bfd08c5f09ee62f2739080
Fiegsy <yassine.daoui031@gmail.com>
2024-01-17 14:09:07 -0800

    Betty resolve

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -7,41 +7,44 @@
  */
 void swap(int *a, int *b)
 {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
+	int temp = *a;
+	*a = *b;
+	*b = temp;
 }
 
 /**
- * hoare_partition - Partitions the array using Hoare partition scheme.
+ * lomuto_partition - Partitions the array using Lomuto partition scheme.
  * @array: Pointer to the array.
  * @low: Starting index of the partition.
  * @high: Ending index of the partition.
  * @size: Size of array.
  * Return: Index of the pivot element.
  */
-int hoare_partition(int *array, int low, int high, size_t size)
+int lomuto_partition(int *array, int low, int high, size_t size)
 {
-    int pivot = array[low + (high - low) / 2];
-    int i = low - 1;
-    int j = high + 1;
+	int pivot = array[high];
+	int i = low - 1;
 
-    while (1)
-    {
-        do {
-            i++;
-        } while (array[i] < pivot);
+	for (int j = low; j <= high - 1; j++)
+	{
+		if (array[j] < pivot)
+		{
+			i++;
+			if (i != j)
+			{
+				swap(&array[i], &array[j]);
+				print_array(array, size);
+			}
+		}
+	}
 
-        do {
-            j--;
-        } while (array[j] > pivot);
+	if (array[i + 1] != array[high])
+	{
+		swap(&array[i + 1], &array[high]);
+		print_array(array, size);
+	}
 
-        if (i >= j)
-            return (j);
-
-        swap(&array[i], &array[j]);
-        print_array(array, size);
-    }
+	return i + 1;
 }
 
 /**
@@ -53,26 +56,26 @@ int hoare_partition(int *array, int low, int high, size_t size)
  */
 void quicksort_recursion(int *array, int low, int high, size_t size)
 {
-    if (low < high)
-    {
-        int pivot_index = hoare_partition(array, low, high, size);
+	if (low < high)
+	{
+		int pivot_index = lomuto_partition(array, low, high, size);
 
-        quicksort_recursion(array, low, pivot_index, size);
-        quicksort_recursion(array, pivot_index + 1, high, size);
-    }
+		quicksort_recursion(array, low, pivot_index - 1, size);
+		quicksort_recursion(array, pivot_index + 1, high, size);
+	}
 }
 
 /**
  * quick_sort - Sorts an array in ascending order
- * using the Quick sort algorithm (Hoare partition).
+ * using the Quick sort algorithm (Lomuto partition).
  * @array: Pointer to the array.
  * @size: Size of the array.
  */
 void quick_sort(int *array, size_t size)
 {
-    if (array == NULL || size < 2)
-        return;
+	if (array == NULL || size < 2)
+		return;
 
-    quicksort_recursion(array, 0, size - 1, size);
+	quicksort_recursion(array, 0, size - 1, size);
 }
 

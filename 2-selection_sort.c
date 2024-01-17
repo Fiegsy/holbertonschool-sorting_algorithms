9465f0ad1e2d63fcb68714a001ecbd62d318e457
Fiegsy <yassine.daoui031@gmail.com>
2024-01-17 04:55:28 -0800

    Made some chnages on the code by adding prin t array size

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -9,26 +9,27 @@
  */
 void selection_sort(int *array, size_t size)
 {
-	if (array == NULL || size < 2)
-		return;
+    if (array == NULL || size < 2)
+        return;
 
-	for (size_t i = 0; i < size - 1; i++)
-	{
-		size_t min_index = i;
+    for (size_t i = 0; i < size - 1; i++)
+    {
+        size_t min_index = i;
 
-		for (size_t j = i + 1; j < size; j++)
-		{
-			if (array[j] < array[min_index])
-				min_index = j;
-		}
+        for (size_t j = i + 1; j < size; j++)
+        {
+            if (array[j] < array[min_index])
+                min_index = j;
+        }
 
-		if (min_index != i)
-		{
-			int temp = array[i];
-			array[i] = array[min_index];
-			array[min_index] = temp;
+        if (min_index != i)
+        {
+            int temp = array[i];
+            array[i] = array[min_index];
+            array[min_index] = temp;
 
-		}
-	}
+            print_array(array, size);
+        }
+    }
 }
 
